e5be28ab7053ce397c6256f2ac8e4da456851aa9
johan196 <johanalvarez196@gmail.com>
2023-04-13 20:58:52 -0500

    COMMIT INICIAL - Creacion de la API REST.

---
+++ src/main/java/org/Primerparcialapp/PrimerparcialAppApplication.java
@@ -0,0 +1,13 @@
+package org.Primerparcialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerparcialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerparcialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/Primerparcialapp/PrimerparcialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Primerparcialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerparcialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
