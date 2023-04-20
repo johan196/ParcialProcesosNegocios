90d5c358871f8de78c0a030bbf7c2b2ff7a7aee4
Giuseppe Tamayo <gtamayoc@ufpso.edu.co>
2023-04-20 07:47:12 -0500

    REFACTORIZACIÓN - Se ajusta error al llamar los get y set de la entidad Car.

--- src/main/java/org/Primerparcialapp/model/Car.java
+++ src/main/java/org/Primerparcialapp/model/Car.java
@@ -25,4 +25,68 @@ public class Car {
     @Column(name = "availability")
     private Boolean availability;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public String getCar_model() {
+        return car_model;
+    }
+
+    public void setCar_model(String car_model) {
+        this.car_model = car_model;
+    }
+
+    public String getCar_color() {
+        return car_color;
+    }
+
+    public void setCar_color(String car_color) {
+        this.car_color = car_color;
+    }
+
+    public int getCar_model_year() {
+        return car_model_year;
+    }
+
+    public void setCar_model_year(int car_model_year) {
+        this.car_model_year = car_model_year;
+    }
+
+    public String getCar_vin() {
+        return car_vin;
+    }
+
+    public void setCar_vin(String car_vin) {
+        this.car_vin = car_vin;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
 }
