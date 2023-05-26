377fd9adb41f30d2de0a5976d90d9ba0d7be8321
johan196 <johanalvarez196@gmail.com>
2023-05-25 23:15:07 -0500

    se crea mensajes  y se crea token

---
+++ src/main/java/org/Primerparcialapp/utils/ApiResponse.java
@@ -0,0 +1,15 @@
+package org.Primerparcialapp.utils;
+
+
+import lombok.Data;
+
+    @Data
+    public class ApiResponse {
+        private String message;
+        private Object data;
+
+        public ApiResponse(String message, Object data) {
+            this.message = message;
+            this.data = data;
+        }
+    }
